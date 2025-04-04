f41bfb8a0e72947951f7d4db90582ab6d84ecd47
tinapayy <kvcelis@up.edu.ph>
2025-04-04 23:33:42 +0800

    feat: create faqs UI

--- bantay-init/tailwind.config.ts
+++ bantay-init/tailwind.config.ts
@@ -44,12 +44,34 @@ export default {
   			input: 'hsl(var(--input))',
   			ring: 'hsl(var(--ring))',
   			'orange-primary': 'hsl(14, 73%, 48%)',
-  			'orange-secondary': 'hsl(19, 100%, 55%)',
+  			'orange-secondary': 'hsl(19, 100%, 55%)'
   		},
   		borderRadius: {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		keyframes: {
+  			'accordion-down': {
+  				from: {
+  					height: '0'
+  				},
+  				to: {
+  					height: 'var(--radix-accordion-content-height)'
+  				}
+  			},
+  			'accordion-up': {
+  				from: {
+  					height: 'var(--radix-accordion-content-height)'
+  				},
+  				to: {
+  					height: '0'
+  				}
+  			}
+  		},
+  		animation: {
+  			'accordion-down': 'accordion-down 0.2s ease-out',
+  			'accordion-up': 'accordion-up 0.2s ease-out'
   		}
   	}
   },
