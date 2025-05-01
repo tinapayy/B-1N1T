71a5777f7b985ac1f709623dae439009c9d9da7d
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2025-05-01 18:34:30 +0800

    feat: add LoRa sender functionality with DHT sensor integration
    SEAN:
    - Implemented a new sender.ino file to handle LoRa data transmission
    - Integrated DHT sensor for temperature and humidity readings
    - Added relay control for power management based on sensor data
    - Established LoRa communication setup with configurable frequency and sync word

---
+++ hardware/sender.ino
@@ -0,0 +1,139 @@
+#include <LoRa.h>
+#include <SPI.h>
+
+//Libraries for LoRa
+#include "DHT.h"
+#define DHTPIN 4 //pin where the dht11 is connected
+DHT dht(DHTPIN, DHT11);
+
+ 
+#define ss 5
+#define rst 14
+#define dio0 2
+
+#define RELAY_PB_L 25      // powerbank to load switch
+#define RELAY_INIT 32      // solar to initial powerbank switch
+#define RELAY_SECOND 33    // solar to second powerbank switch
+
+
+#define SECONDS(x) ((x) * 1000UL)
+
+ 
+int counter = 0;
+int readingID = 0;
+
+String LoRaMessage = "";
+
+float temperature = 0;
+float humidity = 0;
+float heat_index = 0;
+
+unsigned long previousRelayMillis = 0;
+//const unsigned long relayInterval = 2UL * 60UL * 60UL * 1000UL; // 2 hours in milliseconds
+unsigned long relayInterval = 10UL * 1000UL; // 60 seconds in milliseconds
+bool useInitialPowerbank = true;
+
+void startDHT()
+{
+  if (isnan(humidity) || isnan(temperature))
+  {
+  Serial.println("Failed to read from DHT sensor!");
+  return;
+  }
+  Serial.println("DHT initialize!");
+}
+
+//Initialize LoRa module
+void setup() 
+{
+  pinMode(RELAY_INIT, OUTPUT);
+  pinMode(RELAY_SECOND, OUTPUT);
+  pinMode(RELAY_PB_L, OUTPUT);
+
+  Serial.begin(115200); 
+  while (!Serial);
+
+  dht.begin();
+  startDHT();
+
+  Serial.println("LoRa Sender");
+ 
+  LoRa.setPins(ss, rst, dio0);    //setup LoRa transceiver module
+  
+  while (!LoRa.begin(433E6))     //433E6 - Asia, 866E6 - Europe, 915E6 - North America
+  {
+    Serial.println(".");
+    delay(500);
+  }
+  LoRa.setSyncWord(0xA5);
+  Serial.println("LoRa Initializing OK!");
+}
+
+void getReadings(){
+  humidity = dht.readHumidity();
+  temperature = dht.readTemperature();
+  heat_index = dht.computeHeatIndex(temperature, humidity, false);
+  
+  Serial.print(F("Humidity: "));
+  Serial.print(humidity);
+  Serial.print(F("% Temperature: "));
+  Serial.print(temperature);
+  Serial.print(("°C "));
+  Serial.print(F("Heat Index: "));
+  Serial.println((heat_index));
+}
+
+
+
+void loop() 
+{
+  getReadings();
+  LoRaMessage = String(temperature) + ";" + String(humidity)
+              + ";" + String(heat_index) + ";";
+
+  Serial.print("Sending packet: ");
+  Serial.println(counter);
+ 
+  LoRa.beginPacket();   //Send LoRa packet to receiver
+  LoRa.print(LoRaMessage);
+  // LoRa.print(counter);
+  LoRa.endPacket();
+ 
+  readingID++;
+  counter++;
+
+  
+
+  // Check if time to switch the relay
+  unsigned long currentMillis = millis();
+  
+  if (currentMillis - previousRelayMillis >= relayInterval) {
+    previousRelayMillis = currentMillis;
+    if (useInitialPowerbank == true){
+      switchRelayInitial();
+      useInitialPowerbank = false;
+    }else{
+      switchRelaySecondary();
+      useInitialPowerbank = true;
+    }
+    relayInterval = 60UL * 1000UL; // 60 seconds in milliseconds
+
+  }
+  delay(2000);
+}
+
+void switchRelayInitial() {
+  digitalWrite(RELAY_INIT, HIGH);
+  digitalWrite(RELAY_SECOND, LOW);
+  Serial.println("Initial can now charge");
+  delay(2000);
+  digitalWrite(RELAY_PB_L, LOW);
+}
+
+void switchRelaySecondary() {
+  digitalWrite(RELAY_SECOND, HIGH);
+  digitalWrite(RELAY_INIT, LOW);
+  Serial.println("Secondary can now charge");
+  delay(2000);
+  digitalWrite(RELAY_PB_L, HIGH);
+}
